0f0042eddde261da87f76f23670acad37e6b8d3d
toastyyyxd <contact@toastyx.dev>
2025-06-30 10:01:35 +0800

    Enable touch while typing

--- modules/system/global.nix
+++ modules/system/global.nix
@@ -1,4 +1,5 @@
 { config, pkgs, ... }:
 {
   home-manager.backupFileExtension = "hm-backup"; # Set home-manager backup file extension.
+  services.xserver.libinput.touchpad.disableWhileTyping = false;
 }
