9da967ea54431f9f008c16638bada330065eb8c0
toastyyyxd <contact@toastyx.dev>
2025-06-30 09:44:28 +0800

    fix adding pcscd

--- modules/system/yubikey.nix
+++ modules/system/yubikey.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 {
   services.udev.packages = [ pkgs.yubikey-personalization ];
+  services.pcscd.enable = true;
   environment.systemPackages = with pkgs; [
     libfido2
     yubico-pam
     yubikey-manager
     yubico-piv-tool
-    pcscd yubioath-flutter
+    yubioath-flutter
   ];
 }
