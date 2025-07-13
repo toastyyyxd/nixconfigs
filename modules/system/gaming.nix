2dd1d77f519ee66df3cdbcc675e460ae31a517ce
toastyyyxd <contact@toastyx.dev>
2025-07-14 01:35:17 +0800

    proton ge i think

--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -10,6 +10,9 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
   environment.systemPackages = with pkgs; [
     itch
