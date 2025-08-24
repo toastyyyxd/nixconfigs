d43cb7769332597c9f9a489f924386d3dbf3c227
toastyyyxd <contact@toastyx.dev>
2025-08-25 03:25:47 +0800

    freecad

---
+++ modules/features/freecad.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+let
+  freecadOverlay = final: prev: {
+    freecad = (import (pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "f6335dd4a0bb96de60bc8131510147929f2c325f";
+      sha256 = "sha256-x/P3PbUcQ6X9785uV569EiwRreZoVsVYbm00Bj4DNcg=";
+    }) {
+      inherit (final) system;
+      config = final.config;
+    }).freecad;
+  };
+
+  customPkgs = import pkgs.path {
+    inherit (pkgs) system;
+    overlays = [ freecadOverlay ];
+  };
+in {
+  home.packages = with customPkgs; [
+    freecad
+    gmsh calculix-ccx
+    git
+  ];
+}
