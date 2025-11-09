29c7aa695c090a5cb52e2a8ca9b51e69c6457536
toastyyyxd <contact@toastyx.dev>
2025-11-09 22:23:34 +0800

    blabla

--- modules/system/ext-drives.nix
+++ modules/system/ext-drives.nix
@@ -1,6 +1,4 @@
 { config, lib, pkgs, ... }:
 {
-  # My samsung t9 has trouble with UAS when writing.
-  boot.kernelParams = [ "usb-storage.quirks=04e8:61fd:u" ];
   services.udisks2.enable = true; # make sure to use udiskie in home-manager to enable automount
 }
